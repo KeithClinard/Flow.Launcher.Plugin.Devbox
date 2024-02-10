c0ca64ff7bddfd974d2f1eba5abb95cf8e92b87a
Keith <keith@clinard.dev>
2024-02-10 17:15:12 -0500

    feat: Add settings UI (#2)
    * feat: Add settings UI
    
    * feat: Add makefile for local development

---
+++ Flow.Launcher.Plugin.Devbox/Models/Settings.cs
@@ -0,0 +1,12 @@
+﻿namespace Flow.Launcher.Plugin.Devbox.Models
+{
+  public class Settings
+  {
+    public string githubApiToken { get; set; } = null;
+    public string gitFolder { get; set; } = "C:\\git";
+    public string gitWorktreesFolder { get; set; } = "C:\\git\\worktrees";
+    public string wslGitFolder { get; set; } = "/git";
+    public string wslGitWorktreesFolder { get; set; } = "/git/worktrees";
+    public string wslDistroName { get; set; } = "Ubuntu-22.04";
+  }
+}
