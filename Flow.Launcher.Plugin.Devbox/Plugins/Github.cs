e58e136c6d5c2d2d1f1de9b3c546b42ed9388e40
Keith <keith@clinard.dev>
2024-04-20 13:50:12 -0400

    feat: Add PR shortcut (#14)

--- Flow.Launcher.Plugin.Devbox/Plugins/Github.cs
+++ Flow.Launcher.Plugin.Devbox/Plugins/Github.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using Flow.Launcher.Plugin.Devbox.Core;
 
 namespace Flow.Launcher.Plugin.Devbox;
@@ -20,11 +21,12 @@ internal static class Github
     {
       list.Add(new Result
       {
-        Title = "Open Github",
+        Title = "Open Github Pull Requests",
         SubTitle = "...or keep typing to search for repositories",
         Action = (e) =>
         {
-          Helpers.OpenUrl("https://github.com/dashboard");
+          var orgsQueryString = string.Join("+", settings.organizations.Select(org => "+org%3A" + org));
+          Helpers.OpenUrl("https://github.com/pulls?q=is%3Aopen+is%3Apr+NOT+wip+draft%3Afalse+" + orgsQueryString);
           return true;
         },
         IcoPath = _ico
